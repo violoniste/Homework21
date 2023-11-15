4d7e320818eb66b1d56c6d2c14bae8a248ad4570
violoniste <violoniste2@gmail.com>
2023-11-15 21:56:06 +0700

    Initial

---
+++ src/main/java/com/example/homework21/Homework21Application.java
@@ -0,0 +1,13 @@
+package com.example.homework21;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Homework21Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Homework21Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/homework21/Homework21ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework21;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Homework21ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
